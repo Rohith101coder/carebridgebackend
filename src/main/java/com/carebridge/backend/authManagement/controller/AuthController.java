32da1645e0ab6e64ae1cee593574542843fd4bbb
Rohith101coder <rohithvadla07@gmail.com>
2026-06-11 22:56:44 +0530

    health check

--- src/main/java/com/carebridge/backend/authManagement/controller/AuthController.java
+++ src/main/java/com/carebridge/backend/authManagement/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.carebridge.backend.authManagement.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,11 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
     
     private final AuthService authService;
+
+    @GetMapping("/")
+    public String health() {
+        return "CareBridge Backend Running";
+    }
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
